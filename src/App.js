9af5bf1898831ced4c98961fe2099fae06e4b4eb
radhika7171 <aairamahajan@gmail.com>
2021-11-30 09:33:43 -0500

    created bubble component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Bubbles from "./components/bubbles";
+
+function App() {
+  return (
+    <div className="App">
+      <Bubbles />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/bubbles.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Bubbles.css";
+
+function Bubbles() {
+  return (
+    <div>
+      <div id="bubble 1" class="bubble" value="1">
+        p
+      </div>
+      <div id="bubble 2" class="bubble" value="1">
+        p
+      </div>
+      <div id="bubble 3" class="bubble" value="1">
+        p
+      </div>
+
+      <div id="bubble 4" class="bubble" value="1">
+        p
+      </div>
+    </div>
+  );
+}
+
+export default Bubbles;

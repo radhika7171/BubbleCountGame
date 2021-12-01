e3e4845cb7c67d85b2073fbe4244364bc76769b6
radhika7171 <aairamahajan@gmail.com>
2021-11-30 23:44:48 -0500

    acheived some progress

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import Bubbles from "./components/bubbles";
 
 function App() {
   return (
-    <div className="App">
-      <Bubbles />
+    <div className="App" style={{ backgroundColor: "whitesmoke" }}>
+      <div style={{ backgroundColor: "sienna", marginTop: 30, marginLeft: 30 }}>
+        <Bubbles />
+      </div>
+      <div style={{ backgroundColor: "pink" }}>
+        <Bubbles />
+      </div>
+      <div style={{ backgroundColor: "gold" }}>
+        <Bubbles />
+      </div>
+      <div style={{ backgroundColor: "silver" }}>
+        <Bubbles />
+      </div>
     </div>
   );
 }
--- src/components/bubbles.jsx
+++ src/components/bubbles.jsx
@@ -4,17 +4,7 @@ import "./Bubbles.css";
 function Bubbles() {
   return (
     <div>
-      <div id="bubble 1" class="bubble" value="1">
-        p
-      </div>
-      <div id="bubble 2" class="bubble" value="1">
-        p
-      </div>
-      <div id="bubble 3" class="bubble" value="1">
-        p
-      </div>
-
-      <div id="bubble 4" class="bubble" value="1">
+      <div id="bubble 1" class="bubble">
         p
       </div>
     </div>

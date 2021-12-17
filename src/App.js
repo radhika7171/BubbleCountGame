0da451c50abc665481174cb0478153c7dc05a625
radhika7171 <aairamahajan@gmail.com>
2021-12-17 17:37:07 -0500

    update score board using one button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [score, setScore] = useState(0);
+  let number = 100;
+
+  return (
+    <div className="App">
+      <h1>Score Board : {score}</h1>
+      <button className="button" onClick={() => setScore(score + number)}>
+        {number}
+      </button>
+    </div>
+  );
 }
 
 export default App;

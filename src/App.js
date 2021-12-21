f92e640362df4dd0f3d9dd305d71422e0da9599e
radhika7171 <aairamahajan@gmail.com>
2021-12-21 02:29:07 -0500

    update score board using multiple buttons with map function

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import React, { useState } from "react";
 
 function App() {
   const [score, setScore] = useState(0);
-  let number = 100;
+
+  let numbers = [100, 200, 300, 400];
   console.log("current score", score);
 
   return (
     <div className="App">
       <h1>Score Board : {score}</h1>
-      <button className="button" onClick={() => setScore(score + number)}>
-        {number}
+      //
+      {numbers.map((element) => {
+        return (
+          <button className="button" onClick={() => setScore(score + element)}>
+            {element}
+          </button>
+        );
+      })}
+      <button className="button" onClick={() => setScore(score + numbers[0])}>
+        {numbers[0]}
       </button>
-      <button className="button" onClick={() => setScore(score + number)}>
-        {number}
+      <button className="button" onClick={() => setScore(score + numbers[1])}>
+        {numbers[1]}
       </button>
-      <button className="button" onClick={() => setScore(score + number)}>
-        {number}
+      <button className="button" onClick={() => setScore(score + numbers[2])}>
+        {numbers[2]}
+      </button>
+      <button className="button" onClick={() => setScore(score + numbers[3])}>
+        {numbers[3]}
       </button>
     </div>
   );
